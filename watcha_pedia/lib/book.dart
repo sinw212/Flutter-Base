a0a705639fb9a3f9595dde81b6504856cb1bc82c
sinw212 <sinw212@gmail.com>
2023-07-11 12:34:53 +0900

    add feature (change info)

--- watcha_pedia/lib/book.dart
+++ watcha_pedia/lib/book.dart
@@ -4,12 +4,15 @@ class Book {
   String subtitle;
   String thumbnail; // 썸네일 이미지 링크
   String previewLink; // ListTile 을 눌렀을 때 이동하는 링크
+  List authors;
+  String publishedDate;
 
-  Book({
-    required this.id,
-    required this.title,
-    required this.subtitle,
-    required this.thumbnail,
-    required this.previewLink,
-  });
+  Book(
+      {required this.id,
+      required this.title,
+      required this.subtitle,
+      required this.thumbnail,
+      required this.previewLink,
+      required this.authors,
+      required this.publishedDate});
 }
